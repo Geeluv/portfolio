b5c1b65544b843119b38efa5cb8bd721e6551994
Godwin Chinenye Godlove <chinenye.godlove@gmail.com>
2023-12-23 16:24:18 +0100

    added social media links and removed scroll on drop down menu

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const navList = document.querySelector(".nav-list");
 
 menuBtn.addEventListener("click", function () {
     navList.classList.toggle("drop-down");
+    if (navList.classList.contains("drop-down")) {
+        document.body.style.overflowY = "hidden"
+    } else {
+        document.body.style.overflowY = "scroll"
+
+    }
 })
